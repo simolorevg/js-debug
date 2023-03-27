74ae79cb8d4d710e0aa27282326557126bead0a4
simolorevg <simoneviaggi@libero.it>
2023-03-27 16:43:21 +0200

    es 2 3 bundle 2 done

--- bundle-2/main.js
+++ bundle-2/main.js
@@ -21,31 +21,49 @@ for (let i = 0; i < 5; i++) {
  */
 
 // ESERCIZIO 2
-// function addIfEven(num) {
-//     if (num % 2 = 0) {
-//         return num + 5;
-//     }
-//     return num;
-// }
-
+function addIfEven(num) {
+    if (num % 2 == 0) {
+        return num + 5;
+    }
+    return num;
+}
+//RISPOSTE ES 2
+/**
+ 1)L'operatore dell'uguaglianza era scritto in modo errato.
+ */
 
 // ESERCIZIO 3
-// function loopToFive() {
-//     for (let i = 0, i < 5, i++) {
-//         console.log(i);
-//     }
-// }
-
+function loopToFive() {
+    for (let i = 0; i <= 5; i++) {
+        console.log(i);
+    }
+}
+console.log('Segue loop to five');
+loopToFive();
+//RISPOSTE ES 3
+/**
+ 1) Non è stata scritta correttamente la sinstassi del ciclo for, 
+ usando le virgole al posto dei punti e virgola.
+ 2) Non è stat messo il minore uguale, dato che la funzione deve arrivare fino a cinque
+ */
 
 // ESERCIZIO 4 (suggerimento: ci sono 8 errori)
-// function displayEvenNumbers() {
-//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-//     let evenNumbers = [];
-//     for (let i = 0; i < numbers.length - 1; i++;) {
-//         if (numbers % 2 = 0); {
-//             evenNumbers.push(i);
-//         }
-//         return evenNumbers;
-//     }
-// }
-// displayEvenNumbers(); // [2,4,6,8]
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i <= numbers.length -1; i++) {
+        if (numbers[i] % 2 == 0); {
+            evenNumbers.push(numbers[i]);
+        }
+        console.log(evenNumbers);
+        console.log(numbers);
+        return evenNumbers;
+    }
+}
+displayEvenNumbers(); // [2,4,6,8]
+//RISPOSTE ES 4
+/**
+ 1)E' stato aggiunto un punto e virgola di troppo nella sintassi del ciclo for.
+ 2)L'operatore di uguaglianza nel ciclo if, riga 54, era stato scritto in maniera errata: mancava un uguale.
+ 3)nell'if manca l'indice a numbers e nel push ci va messo numbers[i], l'elemento presente all'indice i dell'array numbers.
+ */
