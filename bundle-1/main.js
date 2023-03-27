ed489ea928ebc33a4f961e52865a41ab5e372e6a
simolorevg <simoneviaggi@libero.it>
2023-03-27 16:07:26 +0200

    Exercise 1 done

--- bundle-1/main.js
+++ bundle-1/main.js
@@ -13,55 +13,58 @@
 // ESERCIZIO 1
 function checkAge() {
     const myAge = 32;
-    const message = '';
+    let message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
         message = 'Hai più di 18 anni!';
     }
+    return message;
 }
 checkAge();
+//RISPOSTE ES 1
+//1) Non è presente il return, quindi non restituisce nessun valore
+//2) Message è una costante, quindi non gli si può riassegnare altri valori
+
+// // ESERCIZIO 2
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
 
 
-// ESERCIZIO 2
-function printColorsNumber() {
-    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-}
-printColorsNumber();
+// // ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
 
 
-// ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// // ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = [
+//         'mymail@mail.com',
+//         'yourmail@mail.com',
+//         'hermail@mail.com',
+//         'hismail@mail.com',
+//     ];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+//     let grantAccess = 'false';
 
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = 'true';
+//     }
 
-// ESERCIZIO 4
-function checkAccess() {
-    const addresses = [
-        'mymail@mail.com',
-        'yourmail@mail.com',
-        'hermail@mail.com',
-        'hismail@mail.com',
-    ];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
-
-    let grantAccess = 'false';
-
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
-
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
